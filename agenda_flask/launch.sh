cc7621ab2ca1f042a4563aa4138d46cf497d4348
jerre231 <joaoscofield.20231@poli.ufrj.br>
2023-08-24 21:15:54 -0300

    login-page-done

--- agenda_flask/launch.sh
+++ agenda_flask/launch.sh
@@ -1,1 +1,2 @@
+firefox 127.0.0.1:5000/login &
 flask --app main.py run
--- agenda_flask/main.py
+++ agenda_flask/main.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template, request
+from flask import *
+
+users = {
+    "joao@teste": "password1",
+}
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-        return render_template("index.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email_login")
+        password = request.form.get("pass_login")
+        
+        if email in users and users[email] == password:
+            return redirect(url_for("home"))
+        else:
+            return redirect(url_for("login"))  
+
+    return render_template("login.html")  
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
-        app.run()
+    app.run()
